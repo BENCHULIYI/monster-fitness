ca82e2e3cf06023f13ccc7f71206c2ce7156a682
benchuliyi <benchuliyi@github.com>
2026-06-28 14:53:30 +0800

    修复器材切换功能 + 底部导航栏适配

---
+++ sw.js
@@ -0,0 +1,33 @@
+// Service Worker for Monster Fitness PWA
+const CACHE_NAME = 'monster-fitness-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(names => {
+      return Promise.all(
+        names.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
+  );
+});
